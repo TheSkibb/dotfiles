21b83c9fd17653650e3afd855f90af25256c1ec0
TheSkibb <kfskibrek@icloud.com>
2022-05-21 10:23:46 +0200

    added dotfiles and copying script

---
+++ .vimrc
@@ -0,0 +1,4 @@
+set number
+syntax on
+highlight Search guibg='Purple' guifg='NONE'
+hi Search cterm=NONE ctermfg=grey ctermbg=blue
---
+++ copyFiles.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#Author: Kristian Skibrek
+#Date: 20-05-22
+
+echo "copying files...."
+
+cp -r ../.config ./
+echo "config"
+
+cp -r ../.vimrc ./
+echo "vimrc"
+
+echo "... done"
+
