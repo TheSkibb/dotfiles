f4115c899df7fab53f351d0b37e33fb222fcda84
theskibb <kristianskibrek@protonmail.com>
2025-11-15 10:48:19 +0100

    update date in swaystatus

--- sway/swaystatus.sh
+++ sway/swaystatus.sh
@@ -1,9 +1,10 @@
-date=$(date "+%a %F %H:%M")
+date=$(date "+%a %d/%m (%B) %H:%M")
 
-battery_percentage=$(upower -i $(upower -e | grep 'BAT') | grep -E "percentage" | awk '{print $2}')
+battery_percentage=$(upower -i $(upower -e | grep 'BAT') | grep "percentage" | awk '{print $2}')
+battery_state=$(upower -i $(upower -e | grep 'BAT') | grep "state" | awk '{print $2}')
 
 volume=$(amixer sget Master | awk -F"[][]" '/Left:/ { print $2 }')
-muted=$(amixer -c 0 -D pulse get Master | tail -n 1 | grep -c '\[on\]')
+muted=$(amixer get Master | tail -n 1 | grep -c '\[on\]')
 
 if [ $muted -ne "0" ]; then 
     volume_icon=🔊
@@ -15,9 +16,9 @@ brightness=$(brightnessctl g)
 
 # max brightness could be different for different computers,
 # use "brightnessctl m" to find it
-brightness_max=937
+brightness_max=64764
 
 brightness_percent=$(echo " scale=2; $brightness / $brightness_max * 100" | bc) 
 
 
-echo $brightness_percent % 💡 "|"  $volume $volume_icon "|" $battery_percentage 🔋 "|" $date
+echo $brightness_percent % 💡 "|"  $volume $volume_icon "|" $battery_percentage $battery_state "|" $date
